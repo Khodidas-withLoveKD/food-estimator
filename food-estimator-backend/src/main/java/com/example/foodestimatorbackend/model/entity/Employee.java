adae608f20f440637f156093941e49ca518a25d9
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-12-15 16:57:25 +0530

    Changes to connect to DB;

--- food-estimator-backend/src/main/java/com/example/foodestimatorbackend/model/entity/Employee.java
+++ food-estimator-backend/src/main/java/com/example/foodestimatorbackend/model/entity/Employee.java
@@ -1,7 +1,19 @@
 package com.example.foodestimatorbackend.model.entity;
 
+import lombok.Data;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Data
+@Table(name = "employee")
 public class Employee {
+
+  @Id
+  private int employee_id;
+
+  private String name;
 }
--- food-estimator-backend/src/main/java/com/example/foodestimatorbackend/services/impl/EmployeeServiceImpl.java
+++ food-estimator-backend/src/main/java/com/example/foodestimatorbackend/services/impl/EmployeeServiceImpl.java
@@ -1,4 +1,19 @@
 package com.example.foodestimatorbackend.services.impl;
 
+import com.example.foodestimatorbackend.model.entity.Employee;
+import com.example.foodestimatorbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
 public class EmployeeServiceImpl {
+  @Autowired
+  EmployeeRepository employeeRepository;
+
+  @PostConstruct
+  public void testDBConnection() {
+    List<Employee> listOfEmployees = employeeRepository.findAll();
+    System.out.println("listOfEmployees = " + listOfEmployees);
+  }
 }
