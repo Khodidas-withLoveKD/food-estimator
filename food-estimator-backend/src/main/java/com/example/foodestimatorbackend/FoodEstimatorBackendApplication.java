ac73b3f49d9d5d1d04c12957ce0f8d807fc9ea7a
Khodidas_with_love_KD <chauhan.khodidas@gmail.com>
2022-12-15 16:03:20 +0530

    Init Commit: Created Repo

---
+++ food-estimator-backend/src/main/java/com/example/foodestimatorbackend/FoodEstimatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodestimatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodEstimatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodEstimatorBackendApplication.class, args);
+	}
+
+}
---
+++ food-estimator-backend/src/test/java/com/example/foodestimatorbackend/FoodEstimatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodestimatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodEstimatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
