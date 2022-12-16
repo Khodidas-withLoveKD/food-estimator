8b6d9706cc0ce23f27006bae884c52bef7a5e321
Aniket Modi <aniket.modi@engati.com>
2022-12-16 13:16:54 +0530

    Backend-code-working

--- food-estimator-backend/src/main/java/com/example/foodestimatorbackend/services/impl/EmployeeServiceImpl.java
+++ food-estimator-backend/src/main/java/com/example/foodestimatorbackend/services/impl/EmployeeServiceImpl.java
@@ -46,7 +46,7 @@ public class EmployeeServiceImpl implements EmployeeService {
       if (Objects.nonNull(selectionRequest.getBreakfast())) {
         ArrayList<String> breakfast = selectionRequest.getBreakfast();
 
-        for(int i=0;i<breakfast.size();i++) {
+        for (int i = 0; i < breakfast.size(); i++) {
           switch (Day.valueOf(breakfast.get(i))) {
             case MON:
               headCountRepository.insertIntoHeadCount(employee_id, "BREAKFAST", "MON");
@@ -68,6 +68,70 @@ public class EmployeeServiceImpl implements EmployeeService {
               break;
             case SUN:
               headCountRepository.insertIntoHeadCount(employee_id, "BREAKFAST", "SUN");
+              break;
+            default:
+              break;
+          }
+        }
+      }
+      if (Objects.nonNull(selectionRequest.getLunch())) {
+        ArrayList<String> lunch = selectionRequest.getLunch();
+        for(int i=0;i<lunch.size();i++) {
+          switch (Day.valueOf(lunch.get(i))) {
+            case MON:
+              headCountRepository.insertIntoHeadCount(employee_id, "LUNCH", "MON");
+              break;
+            case TUE:
+              headCountRepository.insertIntoHeadCount(employee_id, "LUNCH", "TUE");
+              break;
+            case WED:
+              headCountRepository.insertIntoHeadCount(employee_id, "LUNCH", "WED");
+              break;
+            case THURS:
+              headCountRepository.insertIntoHeadCount(employee_id, "LUNCH", "THURS");
+              break;
+            case FRI:
+              headCountRepository.insertIntoHeadCount(employee_id, "LUNCH", "FRI");
+              break;
+            case SAT:
+              headCountRepository.insertIntoHeadCount(employee_id, "LUNCH", "SAT");
+              break;
+            case SUN:
+              headCountRepository.insertIntoHeadCount(employee_id, "LUNCH", "SUN");
+              break;
+            default:
+              break;
+
+          }
+
+        }
+      }
+      if (Objects.nonNull(selectionRequest.getDinner())) {
+        ArrayList<String> dinner = selectionRequest.getDinner();
+        for(int i=0;i<dinner.size();i++) {
+          switch (Day.valueOf(dinner.get(i))) {
+            case MON:
+              headCountRepository.insertIntoHeadCount(employee_id, "DINNER", "MON");
+              break;
+            case TUE:
+              headCountRepository.insertIntoHeadCount(employee_id, "DINNER", "TUE");
+              break;
+            case WED:
+              headCountRepository.insertIntoHeadCount(employee_id, "DINNER", "WED");
+              break;
+            case THURS:
+              headCountRepository.insertIntoHeadCount(employee_id, "DINNER", "THURS");
+              break;
+            case FRI:
+              headCountRepository.insertIntoHeadCount(employee_id, "DINNER", "FRI");
+              break;
+            case SAT:
+              headCountRepository.insertIntoHeadCount(employee_id, "DINNER", "SAT");
+              break;
+            case SUN:
+              headCountRepository.insertIntoHeadCount(employee_id, "DINNER", "SUN");
+              break;
+            default:
               break;
 
           }
