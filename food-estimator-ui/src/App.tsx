0ec07be1c0dc8271ab12fe3f84cbcb38848409fc
Aniket Modi <aniket.modi@engati.com>
2022-12-16 19:12:06 +0530

    Selection

--- food-estimator-ui/src/App.tsx
+++ food-estimator-ui/src/App.tsx
@@ -1,12 +1,13 @@
 import NavigationBar from './components/NavigationBar';
 
 import './App.css';
+import MealSelectionPage from './components/MealSelectionPage';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-
+      <MealSelectionPage />
     </div>
   );
 }
---
+++ food-estimator-ui/src/components/MealSelectionPage.tsx
@@ -0,0 +1,37 @@
+import { useStyletron } from 'baseui';
+import { colors } from '../shared/theme';
+import { Select } from "baseui/select";
+import * as React from "react";
+
+
+const MealSelectionPage = () => {
+
+    const [css, theme] = useStyletron()
+    const [value, setValue] = React.useState([]);
+      
+    return (
+        <div className={css({
+            color : colors.primary
+        })}>
+        <Select
+        options={[
+          { label: "Monday", id: "MON" },
+          { label: "Tuesday", id: "TUE" },
+          { label: "Wednesday", id: "WED" },
+          { label: "Thursday", id: "THURS" },
+          { label: "Friday", id: "FRI" },
+          { label: "Saturday", id: "SAT" }
+        ]}
+        value={value}
+        multi
+        placeholder="Select Day"
+        
+      />
+         
+            Selection Page
+        </div>
+    )
+
+}
+
+export default MealSelectionPage
